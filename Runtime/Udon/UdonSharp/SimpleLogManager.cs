8cc5dad91a5ab0079eb38289d913d774b81f381d
Limitex <76650151+Limitex@users.noreply.github.com>
2025-01-22 20:28:27 +0900

    feat: create simple log prefab

---
+++ Runtime/Udon/UdonSharp/SimpleLogManager.cs
@@ -0,0 +1,14 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace Limitex.MonoUI.Udon
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+    public class SimpleLogManager : UdonSharpBehaviour
+    {
+
+    }
+}
