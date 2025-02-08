de91870a4e4ce6a259f458f0c485f58246b8bfa2
Limitex <76650151+Limitex@users.noreply.github.com>
2025-02-08 13:40:34 +0900

    feat: implement MonoUIBehaviour script

--- Editor/Scripts/Build/MonoUIBehaviourEditor.cs
+++ Editor/Scripts/Build/MonoUIBehaviourEditor.cs
@@ -1,11 +1,17 @@
 #if UNITY_EDITOR
 
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 using UnityEditor;
 using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
+using UnityEditor.Events;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using TMPro;
+using VRC.Udon;
+using UdonSharpEditor;
+using Limitex.MonoUI.Udon;
 
 namespace Limitex.MonoUI.Editor.Build
 {
@@ -15,7 +21,94 @@ namespace Limitex.MonoUI.Editor.Build
 
         public void OnProcessScene(Scene scene, BuildReport report)
         {
+            MonoUIBehaviour[] behaviours = Object.FindObjectsOfType<MonoUIBehaviour>(true);
+            foreach (MonoUIBehaviour behaviour in behaviours)
+            {
+                SetupUIEventHandlers(behaviour);
+                EditorUtility.SetDirty(behaviour);
+            }
+        }
 
+        private void SetupUIEventHandlers(MonoUIBehaviour behaviour)
+        {
+            RegisterEventHandler<Button>(behaviour, nameof(MonoUIBehaviour.button), nameof(MonoUIBehaviour.OnButtonClick));
+            RegisterEventHandler<Toggle>(behaviour, nameof(MonoUIBehaviour.toggle), nameof(MonoUIBehaviour.OnToggleValueChanged));
+            RegisterEventHandler<Slider>(behaviour, nameof(MonoUIBehaviour.slider), nameof(MonoUIBehaviour.OnSliderValueChanged));
+            RegisterEventHandler<Scrollbar>(behaviour, nameof(MonoUIBehaviour.scrollbar), nameof(MonoUIBehaviour.OnScrollbarValueChanged));
+            RegisterEventHandler<ScrollRect>(behaviour, nameof(MonoUIBehaviour.scrollRect), nameof(MonoUIBehaviour.OnScrollRectValueChanged));
+            RegisterEventHandler<TMP_Dropdown>(behaviour, nameof(MonoUIBehaviour.tmpDropdown), nameof(MonoUIBehaviour.OnDropdownValueChanged));
+        }
+
+        private void RegisterEventHandler<T>(MonoUIBehaviour behaviour, string fieldName, string eventName) where T : Component
+        {
+            T component = behaviour.GetComponent<T>();
+            if (component == null) return;
+
+            SerializedObject serializedObject = new SerializedObject(behaviour);
+            SerializedProperty property = serializedObject.FindProperty(fieldName);
+            if (property != null)
+            {
+                property.objectReferenceValue = component;
+                serializedObject.ApplyModifiedProperties();
+            }
+            else
+            {
+                Debug.LogError($"Field {fieldName} not found on {behaviour.name}");
+            }
+
+            UdonUIEventSetter eventSetter = new UdonUIEventSetter(component, behaviour);
+            if (eventSetter.IsValid)
+            {
+                eventSetter.SetEvent(eventName);
+            }
+            else
+            {
+                Debug.LogError($"UdonBehaviour not found on {behaviour.name}");
+            }
+        }
+
+        private class UdonUIEventSetter
+        {
+            private readonly Component uiComponent;
+            private readonly UdonBehaviour udonBehaviour;
+
+            public UdonUIEventSetter(Component component, MonoUIBehaviour behaviour)
+            {
+                uiComponent = component;
+                udonBehaviour = UdonSharpEditorUtility.GetBackingUdonBehaviour(behaviour);
+            }
+
+            public bool IsValid => udonBehaviour != null;
+
+            public void SetEvent(string eventName)
+            {
+                if (!IsValid) return;
+
+                UnityEventBase eventBase = GetEventBase();
+                if (eventBase == null) return;
+
+                UnityEventTools.AddStringPersistentListener(
+                    eventBase,
+                    udonBehaviour.SendCustomEvent,
+                    eventName
+                );
+
+                EditorUtility.SetDirty(uiComponent);
+            }
+
+            private UnityEventBase GetEventBase()
+            {
+                return uiComponent switch
+                {
+                    Button button => button.onClick,
+                    Toggle toggle => toggle.onValueChanged,
+                    Slider slider => slider.onValueChanged,
+                    Scrollbar scrollbar => scrollbar.onValueChanged,
+                    ScrollRect scrollRect => scrollRect.onValueChanged,
+                    TMP_Dropdown dropdown => dropdown.onValueChanged,
+                    _ => null
+                };
+            }
         }
     }
 }
--- Runtime/Udon/UdonSharp/MonoUIBehaviour.cs
+++ Runtime/Udon/UdonSharp/MonoUIBehaviour.cs
@@ -1,12 +1,68 @@
-﻿using UdonSharp;
 using UnityEngine;
-using VRC.SDKBase;
-using VRC.Udon;
+using UnityEngine.UI;
+using TMPro;
+using UdonSharp;
 
 namespace Limitex.MonoUI.Udon
 {
+    [DisallowMultipleComponent]
     public class MonoUIBehaviour : UdonSharpBehaviour
     {
+        [HideInInspector] public Button button;
+        [HideInInspector] public Toggle toggle;
+        [HideInInspector] public Slider slider;
+        [HideInInspector] public Scrollbar scrollbar;
+        [HideInInspector] public ScrollRect scrollRect;
+        [HideInInspector] public TMP_Dropdown tmpDropdown;
 
+
+        public void OnButtonClick()
+        {
+            if (button != null) OnButtonClick(button);
+            else Debug.LogWarning("Button reference is null");
+        }
+
+        public void OnToggleValueChanged()
+        {
+            if (toggle != null) OnToggleValueChanged(toggle);
+            else Debug.LogWarning("Toggle reference is null");
+        }
+
+        public void OnSliderValueChanged()
+        {
+            if (slider != null) OnSliderValueChanged(slider);
+            else Debug.LogWarning("Slider reference is null");
+        }
+
+        public void OnScrollbarValueChanged()
+        {
+            if (scrollbar != null) OnScrollbarValueChanged(scrollbar);
+            else Debug.LogWarning("Scrollbar reference is null");
+        }
+
+        public void OnScrollRectValueChanged()
+        {
+            if (scrollRect != null) OnScrollRectValueChanged(scrollRect);
+            else Debug.LogWarning("ScrollRect reference is null");
+        }
+
+        public void OnDropdownValueChanged()
+        {
+            if (tmpDropdown != null) OnDropdownValueChanged(tmpDropdown);
+            else Debug.LogWarning("Dropdown reference is null");
+        }
+
+
+        public virtual void OnButtonClick(Button button) { }
+
+        public virtual void OnToggleValueChanged(Toggle toggle) { }
+
+        public virtual void OnSliderValueChanged(Slider slider) { }
+
+        public virtual void OnScrollbarValueChanged(Scrollbar scrollbar) { }
+
+        public virtual void OnScrollRectValueChanged(ScrollRect scrollRect) { }
+
+        public virtual void OnDropdownValueChanged(TMP_Dropdown dropdown) { }
     }
 }
